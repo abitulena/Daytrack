59868ffa0ed6f411218b93c9a0f184dd9e4905f1
abitulena <lenazihalova800@mail.ru>
2025-10-31 19:56:45 +1000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>DAYTRACK</h3>
+    <p>регистрация</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h3 {
+  font-weight: lighter;
+}
+
+p {
+  color: rgb(58, 42, 52);
+}
+</style>
