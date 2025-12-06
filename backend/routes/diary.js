fdd0eeb2410f1d0bd922df34696b0daf84491a41
leiiyuu <olesia.foks@gmail.com>
2025-12-06 20:07:18 +1000

    Update diary.js

--- backend/routes/diary.js
+++ backend/routes/diary.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import { createEntry, getUserEntries } from '../controllers/diaryController.js';
+import { createEntry, getUserEntries,  updateEntry, deleteEntry, getStats } from '../controllers/diaryController.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.use(authenticateToken);
+
 router.post('/entries', authenticateToken, createEntry);
 
 router.get('/entries', authenticateToken, getUserEntries);
+
+router.put('/entries/:id', updateEntry);
+
+router.delete('/entries/:id', deleteEntry);
+
+router.get('/stats', getStats);
 
 export default router;
